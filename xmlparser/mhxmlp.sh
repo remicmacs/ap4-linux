4a57ae5f32f3a28dfe5a954548fe333f21be7874
remicmacs <rembourgeon@gmail.com>
2019-04-26 11:50:08 +0200

    WIP XML Tag

--- xmlparser/mhxmlp.sh
+++ xmlparser/mhxmlp.sh
@@ -2,7 +2,7 @@
 
 # Function to display usage of script
 function usage () {
-    cat <<EOF
+    cat >&2 <<EOF
 mhxmlp.sh - Horrible Bash XML parser for plant documents
     ---
 usage : cat mydoc.xml | ./mhxmlp.sh
@@ -17,15 +17,103 @@ Please do not report bugs, they are features.
 EOF
 }
 
+# Global boolean flags
+# 0 is true and 1 is false
+catalog_is_open=1
+plant_is_open=1
+closing_tag=1
+
+function is_closing_tag () {
+    echo $1 | grep -q "</"
+}
+
+function is_closed () {
+    if [[ $catalog_is_open -eq 0 ]]; then
+        echo "Tag is open"
+    else
+        echo "Tag is closed"
+    fi
+}
+
+function parse_line () {
+    echo $line;
+
+    # Get tagname by capturing content of the first tag of the line
+    tagname=$(echo $line | sed -rn 's/.*<\/?([^<>/]*)>.*/\1/p')
+
+    # echo "Found tagname \"$tagname\""
+
+    tag_content=$(echo $line | sed -rn "s/.*<$tagname>([^<]*)<\/$tagname>.*/\1/p")
+
+    # echo "Content is : \"$tag_content\""
+
+    case $tagname in
+
+        "CATALOG")
+            # echo "Checking opening/closing tag";
+            is_closing_tag "$line"
+            closing_tag=$?
+
+            if [[ ! $closing_tag -eq catalog_is_open ]]; then
+                parse_error "Closing a closed catalog"
+                exit 19
+            fi
+
+            # DO STG
+
+            # echo "Toggling catalog state";
+            ((catalog_is_open ^= 1));
+            is_closed $catalog_is_open
+        ;;
+        "PLANT")
+            echo "Checking opening/closing tag";
+            is_closing_tag "$line"
+            closing_tag=$?
+
+            if [[ ! $closing_tag -eq plant_is_open ]]; then
+                parse_error "Closing a closed plant entry"
+                exit 19
+            fi
+
+
+            # DO STG
+            # echo "Toggling plant state";
+            ((plant_is_open ^= 1));
+            is_closed $catalog_is_open
+        ;;
+        *) process_tag $tagname "$tag_content";;
+    esac
+
+}
+
+function process_tag () {
+    tagname=$1
+    tag_content=$2
+
+    echo "Tagname is \"$tagname\""
+
+    echo "Content is : \"$tag_content\""
+
+    case $tagname in
+        "COMMON");;
+        "BOTANICAL");;
+        "ZONE");;
+        "LIGHT");;
+        "PRICE");;
+        "AVAILABILITY");;
+        *) parse_error "Unknown tag \"$tagname\"";;
+    esac
+}
+
 # Display error message
 function error_message () {
-    echo $1
+    (>&2 echo $1)
     echo
 }
 
 # Parse error function : displays the error message and exits 19
 function parse_error () {
-    echo "Parse Error: $1"
+    (>&2 echo "Parse Error: $1")
     exit 19
 }
 
@@ -38,8 +126,9 @@ if [ $# -ne 0 ]; then
 fi
 
 # Stripping XML spec tag
+# TODO: Do a while loop until spec is found
 empty_file=false
-read spec_tag|| empty_file=true
+read spec_tag || empty_file=true
 # If there is no such tag, then the XML must be an empty file
 if $empty_file; then
     error_message "Error: Standard input stream was empty"
@@ -47,26 +136,16 @@ if $empty_file; then
     exit 11
 fi
 
-# Check for spec tag
-if $(echo $spec_tag | grep -vq version); then
-    error_message "Parse Error: XML Spec Tag not found"
-    usage
-    exit 19
-fi
+echo "Line found $spec_tag"
+while [[ ! $(echo $spec_tag | grep -q '<?') ]]; do
+    read spec_tag || parse_error "XML Spec Tag not found" || exit 19
+    echo "Line found $spec_tag"
+done
 
 line_nb=0
 plants_nb=0
 while read line; do
-    echo $line;
-
-    # Get tagname by capturing content of the first tag of the line
-    tagname=$(echo $line | sed -rn 's/.*<\/?([^<>/]*)>.*/\1/p')
-
-    echo "Found tagname \"$tagname\""
-
-    tag_content=$(echo $line | sed -rn "s/.*<$tagname>([^<]*)<\/$tagname>.*/\1/p")
-
-    echo "Content is : \"$tag_content\""
+    parse_line $line
     line_nb=$((line_nb + 1))
 done
 
